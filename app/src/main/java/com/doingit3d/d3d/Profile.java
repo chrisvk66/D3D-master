32796b0865c03db7a4e3cfc236f5bec770ed7ee0
david2319 <david.martin.molina@gmail.com>
2017-04-14 13:24:15 +0200

    He creado la actividad de "como funciona" y su funcionalidad de deslizar hacia los lados las imagenes

---
+++ app/src/main/java/com/doingit3d/d3d/Profile.java
@@ -0,0 +1,17 @@
+package com.doingit3d.d3d;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by David M on 14/04/2017.
+ */
+
+public class Profile extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+
+
+    }
+}
