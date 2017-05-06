9a93e05fef901b8ba4d5a045975b1b388380187c
david2319 <david.martin.molina@gmail.com>
2017-05-06 12:50:47 +0200

    He creado el layout de perfil pero fallan cosas y no se porque

--- app/src/main/java/com/doingit3d/d3d/How_does_it_works.java
+++ app/src/main/java/com/doingit3d/d3d/How_does_it_works.java
@@ -1,14 +1,19 @@
 package com.doingit3d.d3d;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by David M on 11/04/2017.
  */
 
-public class How_does_it_works extends AppCompatActivity {
+public class How_does_it_works extends MainActivity {
     private CustomSwipeAdapter csa;
     private ViewPager vp;
 
@@ -20,9 +25,22 @@ public class How_does_it_works extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.how_does_it_works);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         vp =(ViewPager)findViewById(R.id.viewpager);
         csa= new CustomSwipeAdapter(How_does_it_works.this,fotos);
         vp.setAdapter(csa);
 
+
     }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+
+        startActivity(new Intent(this,MainActivity.class));
+    }
+
+
 }
